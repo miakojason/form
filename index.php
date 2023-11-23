9902de2152a19d3cca761caf78951e48f1794ef0
miakojason <miakojason@gmail.com>
2023-11-23 12:26:02 +0800

    form更新

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,10 +8,20 @@
     <title>PHP的檔案處理</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<a href="upload.php"><div class="types">表單檔案上傳</div></a>
-<a href="manage.php"><div class="types">檔案管理</div></a>
-<a href="text-import.php"><div class="types">文字檔案處理</div></a>
-<a href="image.php"><div class="types">圖形處理</div></a>
+    <a href="upload.php">
+        <div class="types">表單檔案上傳</div>
+    </a>
+    <a href="manage.php">
+        <div class="types">檔案管理</div>
+    </a>
+    <a href="text-import.php">
+        <div class="types">文字檔案處理</div>
+    </a>
+    <a href="image.php">
+        <div class="types">圖形處理</div>
+    </a>
 </body>
+
 </html>
