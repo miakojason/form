b5784e167cef5ed2fbeab4a662b7edb2e7a836fe
miakojason <miakojason@gmail.com>
2024-02-21 13:56:30 +0800

    使用PHP程式進行資料夾的瀏灠和檔案處理

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,20 +7,12 @@
     <title>PHP的檔案處理</title>
     <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
-    <a href="upload.php">
-        <div class="types">表單檔案上傳</div>
-    </a>
-    <a href="manage.php">
-        <div class="types">檔案管理</div>
-    </a>
-    <a href="text-import.php">
-        <div class="types">文字檔案處理</div>
-    </a>
-    <a href="image.php">
-        <div class="types">圖形處理</div>
-    </a>
+<a href="upload.php"><div class="types">表單檔案上傳</div></a>
+<a href="manage.php"><div class="types">檔案管理</div></a>
+<a href="text-import.php"><div class="types">文字檔案處理</div></a>
+<a href="image.php"><div class="types">圖形處理-縮圖</div></a>
+<a href="image2.php"><div class="types">圖形處理-加邊框</div></a>
+<a href="filelist.php"><div class="types">檔案處理-瀏灠資料夾</div></a>
 </body>
-
 </html>
