5957602933f1a875017f38ebbc65524afac0b345
miakojason <miakojason@gmail.com>
2024-02-23 09:14:45 +0800

    自行錯誤bool

---
+++ index copy.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP的檔案處理</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<a href="upload.php"><div class="types">表單檔案上傳</div></a>
+<a href="manage.php"><div class="types">檔案管理</div></a>
+<a href="text-import.php"><div class="types">文字檔案處理</div></a>
+<a href="image.php"><div class="types">圖形處理-縮圖</div></a>
+<a href="image2.php"><div class="types">圖形處理-加邊框</div></a>
+<a href="filelist.php"><div class="types">檔案處理-瀏灠資料夾</div></a>
+<a href="catpcha.php"><div class="types">圖形處理-圖形驗證碼</div></a>
+</body>
+</html>
